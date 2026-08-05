ed6293b7bd5f5b4ec36dcc2ecc61b4e958b09c85
priya-mishra007 <shashimaa87@gmail.com>
2026-08-05 22:07:04 +0530

    Time: 53 ms (93.27%), Space: 293.9 MB (21.16%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] e : invocations) {
+            graph[e[0]].add(e[1]);
+        }
+        boolean[] sus = new boolean[n];
+        dfs(k, graph, sus);
+        for (int[] e : invocations) {
+            int u = e[0];
+            int v = e[1];
+            if (!sus[u] && sus[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!sus[i]) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int node, List<Integer>[] graph, boolean[] sus) {
+        sus[node] = true;
+        for (int next : graph[node]) {
+            if (!sus[next]) {
+                dfs(next, graph, sus);
+            }
+        }
+    }
+}
