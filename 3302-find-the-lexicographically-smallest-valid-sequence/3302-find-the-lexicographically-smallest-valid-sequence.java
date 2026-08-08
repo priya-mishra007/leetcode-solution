0d918152e9053999cc9c8b5d49675d340fce5e6b
priya-mishra007 <shashimaa87@gmail.com>
2026-08-08 18:38:28 +0530

    Time: 35 ms (69.44%), Space: 136.9 MB (86.11%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -4,21 +4,22 @@ class Solution {
         int m = word2.length();
         int[] suf = new int[n + 1];
         int j = m - 1;
-        for(int i = n - 1; i >= 0; i--){
-            if(j >= 0 && word1.charAt(i) == word2.charAt(j))
+        for (int i = n - 1; i >= 0; i--) {
+            suf[i] = suf[i + 1];
+            if (j >= 0 && word1.charAt(i) == word2.charAt(j)) {
+                suf[i]++;
                 j--;
-            suf[i] = m - 1 - j;
+            }
         }
         int[] ans = new int[m];
         int k = 0;
         j = 0;
         boolean used = false;
-        for(int i = 0; i < n && k < m; i++){
-            if(word1.charAt(i) == word2.charAt(j)){
+        for (int i = 0; i < n && k < m; i++) {
+            if (word1.charAt(i) == word2.charAt(j)) {
                 ans[k++] = i;
                 j++;
-            }
-            else if (!used && suf[i + 1] >= m - j -1){
+            } else if (!used && j < m && suf[i + 1] >= m - j - 1) {
                 ans[k++] = i;
                 j++;
                 used = true;
