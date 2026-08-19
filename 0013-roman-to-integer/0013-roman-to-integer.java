6fd0eafa35c6627be1fd6bceb281ff519d9d43e6
priya-mishra007 <shashimaa87@gmail.com>
2026-08-19 19:43:04 +0530

    Time: 3 ms (77.56%), Space: 46.2 MB (94%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,25 +2,45 @@ class Solution {
     public int romanToInt(String s) {
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
-            int current = value(s.charAt(i));
-            if (i + 1 < s.length() && current < value(s.charAt(i + 1))) {
-                result -= current;
-            } else {
-                result += current;
+            char current = s.charAt(i);
+            int currentValue = 0;
+            if (current == 'I')
+                currentValue = 1;
+            else if (current == 'V')
+                currentValue = 5;
+            else if (current == 'X')
+                currentValue = 10;
+            else if (current == 'L')
+                currentValue = 50;
+            else if (current == 'C')
+                currentValue = 100;
+            else if (current == 'D')
+                currentValue = 500;
+            else if (current == 'M')
+                currentValue = 1000;
+            int nextValue = 0;
+            if (i + 1 < s.length()) {
+                char next = s.charAt(i + 1);
+                if (next == 'I')
+                    nextValue = 1;
+                else if (next == 'V')
+                    nextValue = 5;
+                else if (next == 'X')
+                    nextValue = 10;
+                else if (next == 'L')
+                    nextValue = 50;
+                else if (next == 'C')
+                    nextValue = 100;
+                else if (next == 'D')
+                    nextValue = 500;
+                else if (next == 'M')
+                    nextValue = 1000;
             }
+            if (currentValue < nextValue)
+                result -= currentValue;
+            else
+                result += currentValue;
         }
         return result;
-    }
-    private int value(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-        }
-        return 0;
     }
 }
